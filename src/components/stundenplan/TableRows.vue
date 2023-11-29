fe8ec9358ed6b5fa62176a623b3e38bc53f65c94
TheOliveOli38 <theoliveoli38@gmail.com>
2023-11-29 11:13:33 +0100

    Fixed TableRows

--- src/components/stundenplan/TableRows.vue
+++ src/components/stundenplan/TableRows.vue
@@ -1,11 +1,31 @@
 <template>
-  <tr v-for="index in 8" :key="index">
-    <th>{{ index }}. Std</th>
-    <td>{{ getSubjectByIndex(index, montagFach) }}</td>
-    <td>{{ getSubjectByIndex(index, dienstagFach) }}</td>
-    <td>{{ getSubjectByIndex(index, mittwochFach) }}</td>
-    <td>{{ getSubjectByIndex(index, donnerstagFach) }}</td>
-    <td>{{ getSubjectByIndex(index, freitagFach) }}</td>
+  <tr v-for="(s, index) in std" :key="index">
+    <th>{{ index + 1 }}. Std</th>
+    <td>
+      {{ getFachByWochentag(s, "montag") }} <br />
+      {{ getLehrerByWochentag(s, "montag") }} |
+      {{ getRaumByWochentag(s, "montag") }}
+    </td>
+    <td>
+      {{ getFachByWochentag(s, "dienstag") }} <br />
+      {{ getLehrerByWochentag(s, "dienstag") }} |
+      {{ getRaumByWochentag(s, "dienstag") }}
+    </td>
+    <td>
+      {{ getFachByWochentag(s, "mittwoch") }} <br />
+      {{ getLehrerByWochentag(s, "mittwoch") }} |
+      {{ getRaumByWochentag(s, "mittwoch") }}
+    </td>
+    <td>
+      {{ getFachByWochentag(s, "donnerstag") }} <br />
+      {{ getLehrerByWochentag(s, "donnerstag") }} |
+      {{ getRaumByWochentag(s, "donnerstag") }}
+    </td>
+    <td>
+      {{ getFachByWochentag(s, "freitag") }} <br />
+      {{ getLehrerByWochentag(s, "freitag") }} |
+      {{ getRaumByWochentag(s, "freitag") }}
+    </td>
   </tr>
 </template>
 
@@ -13,19 +33,22 @@
 import { mapGetters } from "vuex";
 
 export default {
+  data() {
+    return {
+      std: ["std1", "std2", "std3", "std4", "std5", "std6", "std7", "std8"],
+    };
+  },
   computed: {
     ...mapGetters([
-      "getStd1Subjects",
-      // "getStdDienstag",
-      // "getStdMittwoch",
-      // "getStdDonnerstag",
-      // "getStdFreitag",
+      "getFachByWochentag",
+      "getLehrerByWochentag",
+      "getRaumByWochentag",
     ]),
   },
-  methods: {
-    getSubjectByIndex(index, day) {
-      this.$store.getters[`getStd${index}Subjects`][day];
-    },
-  },
+  // methods: {
+  //   getSubjectByIndex(index) {
+  //     this.$store.getters[`getStd${index}Subjects`];
+  //   },
+  // },
 };
 </script>
